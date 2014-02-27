79a806318020133f46992ac771922b53eb09adf3
Ryan Moore <dexa187@gmail.com>
2014-02-26 21:44:52 -0500

    added basic auth

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,56 @@
 package controllers;
 
+import models.User;
 import play.Routes;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+	@Security.Authenticated(Secured.class)
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+    
+    public static Result login() {
+        return ok(login.render(Form.form(Login.class)));
+    }
+    
+    public static Result authenticate() {
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+    
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+            routes.Application.login()
+        );
+    }
+    
+    public static class Login {
+
+        public String email;
+        public String password;
+        
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+              return "Invalid user or password";
+            }
+            return null;
+        }
+
     }
     
     public static Result javascriptRoutes() {
