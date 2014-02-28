98683f3c29d336821bac53a9fa018c2ab912bcd6
Ryan Moore <dexa187@gmail.com>
2014-02-27 23:10:49 -0500

    Added Auth to api

--- app/controllers/MqttBridge.java
+++ app/controllers/MqttBridge.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import actions.BasicAuth;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -10,35 +12,29 @@ import services.MqttService;
 import views.html.*;
 
 public class MqttBridge extends Controller {
+	
+	@BasicAuth
 	public static Result listTopics() throws Exception {
-		JsonNode json = request().body().asJson();
 		MqttService mqtt = new MqttService();
 		return ok(mqtt.subscribe("#"));
 	}
 	
+	@BasicAuth
 	public static Result getTopic(String topic) throws Exception{
-		JsonNode json = request().body().asJson();
 		MqttService mqtt = new MqttService();
 		return ok(mqtt.subscribe(topic));
 	}
 	
+	@BasicAuth
 	public static Result publish(String topic) throws Exception{
 		MqttService mqtt = new MqttService();
 		String payload = request().body().asText();
-		System.out.println("this is the payload" +request().toString());
-		if(mqtt.publish(topic, payload, true)){
-			return ok();
-		}else{
-			return internalServerError();
-		}
+		return (mqtt.publish(topic, payload, true)) ? ok() : internalServerError();
 	}
 	
+	@BasicAuth
 	public static Result delete(String topic) throws Exception {
 		MqttService mqtt = new MqttService();
-		if(mqtt.delete(topic)){
-			return ok();
-		}else{
-			return internalServerError();
-		}
+		return (mqtt.delete(topic)) ? ok() : internalServerError();
 	}
 }
